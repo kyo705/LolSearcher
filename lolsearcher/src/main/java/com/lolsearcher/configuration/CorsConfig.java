12bec79a730adc28860828da848569f09568d9ae
kyo705 <mission0705@naver.com>
2022-10-29 02:33:06 +0900

    [ADD] OPEN API 서비스에 대한 CORS 정책 설정

---
+++ lolsearcher/src/main/java/com/lolsearcher/configuration/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.lolsearcher.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	CorsFilter oepnApiCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowCredentials(true);
+		corsConfig.addAllowedOrigin("*");
+		corsConfig.addAllowedMethod("GET");
+		corsConfig.addAllowedHeader("*");
+		
+		source.registerCorsConfiguration("/api/**", corsConfig);
+		
+		return new CorsFilter(source);
+	}
+}
--- lolsearcher/src/main/java/com/lolsearcher/configuration/SecurityConfig.java
+++ lolsearcher/src/main/java/com/lolsearcher/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.lolsearcher.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.web.filter.CorsFilter;
 
 import com.lolsearcher.auth.exceptiontranslationfilter.LolsearcherDeniedHandler;
 import com.lolsearcher.auth.usernamepassword.UserLoginFailHandler;
@@ -23,6 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Qualifier("openApiCorsFilter")
+	private CorsFilter openApiCorsFilter;
 	
 	@Bean
 	public OauthUserService oauthUserService() {
@@ -49,7 +54,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		
-		http.authorizeRequests()
+		http.addFilter(openApiCorsFilter)
+			.authorizeRequests()
 				.antMatchers("/api/**").access("hasRole('ROLE_GET')")
 				.anyRequest().permitAll()
 				.and()
