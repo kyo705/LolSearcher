ec6f4399b14da05a553b9c63ed1fa70cafcd6a19
kyo705 <mission0705@naver.com>
2022-11-04 13:18:07 +0900

    [REFACTOR] OauthUserSevice 기능 모듈화
    하나의 메소드를 기능별로 여러 메소드로 나눔

--- lolsearcher/src/main/java/com/lolsearcher/service/OauthUserService.java
+++ lolsearcher/src/main/java/com/lolsearcher/service/OauthUserService.java
@@ -2,7 +2,8 @@ package com.lolsearcher.service;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -15,8 +16,9 @@ import com.lolsearcher.auth.usernamepassword.LolsearcherUserDetails;
 import com.lolsearcher.domain.entity.user.LolSearcherUser;
 import com.lolsearcher.repository.userrepository.UserRepository;
 
+@Service
 public class OauthUserService extends DefaultOAuth2UserService {
-	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	private UserRepository userRepository;
 
@@ -32,26 +34,43 @@ public class OauthUserService extends DefaultOAuth2UserService {
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		
 		OAuth2User oAuth2User = super.loadUser(userRequest);
+		LolSearcherUser user = findLolSearcherAccount(oAuth2User);
+		logger.info("로그인된 lolsearcher user : '{}'", user);
+		
 		Map<String, Object> attributes = oAuth2User.getAttributes();
-		System.out.println(oAuth2User);
+		LolsearcherUserDetails lolseracherUserDetails = new LolsearcherUserDetails(user, attributes);
+		
+		return lolseracherUserDetails;
+	}
+	
+	private LolSearcherUser findLolSearcherAccount(OAuth2User oAuth2User) {
+		String username = (String)oAuth2User.getAttributes().get("sub");
+		String email = (String) oAuth2User.getAttributes().get("email");
+		
+		LolSearcherUser user = userRepository.findUserByName(username);
+		if(user!=null) {
+			user.setLastLoginTimeStamp(System.currentTimeMillis());
+			return user;
+		}
+		
+		user = userRepository.findUserByEmail(email);
+		if(user!=null) {
+			user.setLastLoginTimeStamp(System.currentTimeMillis());
+			return user;
+		}
+		
+		user = createLolSearcherAccount(oAuth2User);
+		user.setLastLoginTimeStamp(System.currentTimeMillis());
+		return user;
+	}
+	
+	
+	private LolSearcherUser createLolSearcherAccount(OAuth2User oAuth2User) {
 		String username = (String)oAuth2User.getAttributes().get("sub");
 		String password = bCryptPasswordEncoder.encode(username+System.currentTimeMillis());
 		String role = "ROLE_GET";
 		String email = (String) oAuth2User.getAttributes().get("email");
 		
-		LolSearcherUser user = userRepository.findUserByName(username);
-		
-		if(user==null) {
-			user = userRepository.findUserByEmail(email);
-			if(user==null) {
-				user = new LolSearcherUser(username, password, role, email, 0);
-				userRepository.saveUser(user);
-			}
-		}
-		
-		user.setLastLoginTimeStamp(System.currentTimeMillis());
-		LolsearcherUserDetails lolseracherUserDetails = new LolsearcherUserDetails(user, attributes);
-		
-		return lolseracherUserDetails;
+		return new LolSearcherUser(username, password, role, email, 0);
 	}
 }
